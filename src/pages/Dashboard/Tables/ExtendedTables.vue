e2faa1fd042ef939b855d60904f506a6e53f28d0
Josia Almeida <josiaalmeida0001@gmail.com>
2021-04-18 17:43:59 +0100

    Alterando Tabela

--- src/pages/Dashboard/Tables/ExtendedTables.vue
+++ src/pages/Dashboard/Tables/ExtendedTables.vue
@@ -1,65 +1,184 @@
 <template>
   <div class="md-layout">
-    <div class="md-layout-item md-size-100">
+    <div class="md-layout-item">
       <md-card>
         <md-card-header class="md-card-header-icon md-card-header-green">
           <div class="card-icon">
             <md-icon>assignment</md-icon>
           </div>
-          <h4 class="title">Marcações</h4>
+          <h4 class="title">Alunos</h4>
         </md-card-header>
         <md-card-content>
-          <md-table v-model="tableData">
+          <md-table
+            :value="queriedData"
+            :md-sort.sync="currentSort"
+            :md-sort-order.sync="currentSortOrder"
+            :md-sort-fn="customSort"
+            class="paginated-table table-striped table-hover"
+          >
+            <md-table-toolbar>
+              <md-field>
+                <label for="pages">Per page</label>
+                <md-select v-model="pagination.perPage" name="pages">
+                  <md-option
+                    v-for="item in pagination.perPageOptions"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  >
+                    {{ item }}
+                  </md-option>
+                </md-select>
+              </md-field>
+
+              <md-field>
+                <md-input
+                  type="search"
+                  class="mb-3"
+                  clearable
+                  style="width: 200px"
+                  placeholder="Search records"
+                  v-model="searchQuery"
+                >
+                </md-input>
+              </md-field>
+            </md-table-toolbar>
             <md-table-row slot="md-table-row" slot-scope="{ item }">
-              <md-table-cell md-label="Id">{{ item.id }}</md-table-cell>
-              <md-table-cell md-label="Aluno">{{ item.name }}</md-table-cell>
-              <md-table-cell md-label="Aula">{{
-                item.country
+              <md-table-cell md-label="Aluno" md-sort-by="name">{{
+                item.name
               }}</md-table-cell>
-              <md-table-cell md-label="Data">{{ item.data }}</md-table-cell>
-              <md-table-cell md-label="Lição">{{ item.salary }}</md-table-cell>
-              <md-table-cell md-label="Actions" :class="getAlignClasses(item)">
+              <md-table-cell md-label="Email" md-sort-by="email">{{
+                item.email
+              }}</md-table-cell>
+              <md-table-cell md-label="Idade"> {{ item.age }}</md-table-cell>
+              <!-- <md-table-cell md-label="Salary">{{ item.salary }}</md-table-cell> -->
+              <md-table-cell md-label="Actions">
                 <md-button
-                  class="md-just-icon"
-                  :class="getClass(item.icon1, item.id)"
-                  ><md-icon>{{ item.icon1 }}</md-icon></md-button
+                  class="md-just-icon md-info md-simple"
+                  @click.native="handleLike(item)"
                 >
+                  <md-icon>favorite</md-icon>
+                </md-button>
                 <md-button
-                  class="md-just-icon"
-                  :class="getClass(item.icon2, item.id)"
-                  ><md-icon>{{ item.icon2 }}</md-icon></md-button
+                  class="md-just-icon md-warning md-simple"
+                  @click.native="handleEdit(item)"
                 >
+                  <md-icon>dvr</md-icon>
+                </md-button>
                 <md-button
-                  class="md-just-icon"
-                  :class="getClass(item.icon3, item.id)"
-                  @click.native="handleDelete(item, item.id)"
-                  ><md-icon>{{ item.icon3 }}</md-icon></md-button
+                  class="md-just-icon md-danger md-simple"
+                  @click.native="handleDelete(item)"
                 >
+                  <md-icon>close</md-icon>
+                </md-button>
               </md-table-cell>
             </md-table-row>
           </md-table>
+          <div class="footer-table md-table">
+            <table>
+              <tfoot>
+                <tr>
+                  <th
+                    v-for="item in footerTable"
+                    :key="item.name"
+                    class="md-table-head"
+                  >
+                    <div class="md-table-head-container md-ripple md-disabled">
+                      <div class="md-table-head-label">
+                        {{ item }}
+                      </div>
+                    </div>
+                  </th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
         </md-card-content>
+        <md-card-actions md-alignment="space-between">
+          <div class="">
+            <p class="card-category">
+              Showing {{ from + 1 }} to {{ to }} of {{ total }} entries
+            </p>
+          </div>
+          <pagination
+            class="pagination-no-border pagination-success"
+            v-model="pagination.currentPage"
+            :per-page="pagination.perPage"
+            :total="total"
+          >
+          </pagination>
+        </md-card-actions>
       </md-card>
     </div>
   </div>
 </template>
+
 <script>
-import marcacao from './marcacao'
+import { Pagination } from "@/components";
+import marcacao from "./marcacao";
+import Fuse from "fuse.js";
 import Swal from "sweetalert2";
+// import userModule from '../../../store/modules/userModule'
+
 export default {
-  data() {
-    return {
-      tableData: marcacao,
-    };
+  components: {
+    Pagination
   },
   computed: {
-    shoppingTotal() {
-      return this.shoppingCartTable.reduce((accumulator, current) => {
-        return accumulator + current.amount;
-      }, 0);
+    /***
+     * Returns a page from the searched data or the whole data. Search is performed in the watch section below
+     */
+    queriedData() {
+      let result = this.tableData;
+      if (this.searchedData.length > 0) {
+        result = this.searchedData;
+      }
+      return result.slice(this.from, this.to);
+    },
+    to() {
+      let highBound = this.from + this.pagination.perPage;
+      if (this.total < highBound) {
+        highBound = this.total;
+      }
+      return highBound;
+    },
+    from() {
+      return this.pagination.perPage * (this.pagination.currentPage - 1);
+    },
+    total() {
+      return this.searchedData.length > 0
+        ? this.searchedData.length
+        : this.tableData.length;
     }
   },
+  data() {
+    return {
+      currentSort: "name",
+      currentSortOrder: "asc",
+      pagination: {
+        perPage: 5,
+        currentPage: 1,
+        perPageOptions: [5, 10, 25, 50],
+        total: 0
+      },
+      footerTable: ["Name", "Email", "Idade",  "Actions"],
+      searchQuery: "",
+      propsToSearch: ["name", "email", "age"],
+      tableData: marcacao,
+      searchedData: [],
+      fuseSearch: null
+    };
+  },
   methods: {
+    customSort(value) {
+      return value.sort((a, b) => {
+        const sortBy = this.currentSort;
+        if (this.currentSortOrder === "desc") {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
+        return b[sortBy].localeCompare(a[sortBy]);
+      });
+    },
     handleLike(item) {
       Swal.fire({
         title: `You liked ${item.name}`,
@@ -75,7 +194,7 @@ export default {
         confirmButtonClass: "md-button md-info"
       });
     },
-    handleDelete(item, id) {
+    handleDelete(item) {
       Swal.fire({
         title: "Are you sure?",
         text: `You won't be able to revert this!`,
@@ -87,7 +206,7 @@ export default {
         buttonsStyling: false
       }).then(result => {
         if (result.value) {
-          this.deleteRow(id);
+          this.deleteRow(item);
           Swal.fire({
             title: "Deleted!",
             text: `You deleted ${item.name}`,
@@ -105,101 +224,36 @@ export default {
       if (indexToDelete >= 0) {
         this.tableData.splice(indexToDelete, 1);
       }
-    },
-    getClass: function(item, id) {
-      let classes = "";
-      switch (item) {
-        case "person": {
-          classes = "md-info";
-          break;
-        }
-        case "edit": {
-          classes = "md-success";
-          break;
-        }
-        case "close": {
-          classes = "md-danger";
-          break;
-        }
+    }
+  },
+  mounted() {
+    // Fuse search initialization.
+    this.fuseSearch = new Fuse(this.tableData, {
+      keys: ["name", "email"],
+      threshold: 0.3
+    });
+  },
+  watch: {
+    /**
+     * Searches through the table data by a given query.
+     * NOTE: If you have a lot of data, it's recommended to do the search on the Server Side and only display the results here.
+     * @param value of the query
+     */
+    searchQuery(value) {
+      let result = this.tableData;
+      if (value !== "") {
+        result = this.fuseSearch.search(this.searchQuery);
       }
-      switch (id) {
-        case 1:
-        case 5: {
-          break;
-        }
-        case 2:
-        case 4: {
-          classes = `${classes} md-round`;
-          break;
-        }
-        case 3: {
-          classes = `${classes} md-simple`;
-          break;
-        }
-      }
-      return classes;
-    },
-    getAlignClasses: ({ id }) => ({
-      "text-right": id
-    }),
-    increaseQuantity(item) {
-      item.qty++;
-      this.computeAmount(item);
-    },
-    decreaseQuantity(item) {
-      if (item.qty > 1) {
-        item.qty--;
-        this.computeAmount(item);
-      }
-    },
-    computeAmount(item) {
-      item.amount = item.qty * item.price;
+      this.searchedData = result;
     }
   }
 };
 </script>
-<style lang="scss" scoped>
-.text-right .md-table-cell-container {
-  display: flex;
-  justify-content: flex-end;
-}
-.md-table .md-table-head:last-child {
-  text-align: right;
-}
 
-.table-stats {
-  display: flex;
-  align-items: center;
-  text-align: right;
-  flex-flow: row wrap;
-
-  .td-price .td-total {
-    display: inline-flex;
-    font-weight: 500;
-    font-size: 1.0625rem;
-    margin-right: 50px;
-  }
-
-  &.table-striped .td-price {
-    border-bottom: 0;
-  }
-
-  .td-price {
-    font-size: 26px;
-    border-top: 1px solid #ddd;
-    border-bottom: 1px solid #ddd;
-  }
-
-  .td-price,
-  > div {
-    flex: 0 0 100%;
-    padding: 12px 8px;
-  }
-}
-
-.table-shopping .md-table-head:nth-child(5),
-.table-shopping .md-table-head:nth-child(7),
-.table-shopping .md-table-head:nth-child(6) {
-  text-align: right;
+<style lang="css" scoped>
+.md-card .md-card-actions {
+  border: 0;
+  margin-left: 20px;
+  margin-right: 20px;
 }
 </style>
