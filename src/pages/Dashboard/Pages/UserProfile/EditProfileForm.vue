05c645e33802fa5a805213e281ebf7b7b5071413
Daniel Almeida <daniel20150154@gmail.com>
2021-05-19 11:29:46 +0100

    Remove console.logs git ignore dist directorie

--- src/pages/Dashboard/Pages/UserProfile/EditProfileForm.vue
+++ src/pages/Dashboard/Pages/UserProfile/EditProfileForm.vue
@@ -82,7 +82,7 @@ export default {
       return "md-card-header-" + headerColor + "";
     },
     setNames(nome) {
-      console.log(nome);
+      // console.log(nome);
       if (nome !== null) {
         const names = nome.split(" ");
         this.user.firstName = names[0];
