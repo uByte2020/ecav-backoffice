85766748a5141be5ebc2c15bd03b8e4eb256a274
Atila <atilaesousa@gmail.com>
2021-06-05 23:01:46 +0100

    feature: página 404 adicionada

---
+++ src/pages/Dashboard/Pages/NotFound.vue
@@ -0,0 +1,23 @@
+<template >
+  <div class="notfound">
+    <h1>Página não encontrada</h1>
+    <md-button class="md-info" @click="goback()">Voltar</md-button>
+  </div>
+</template>
+<script>
+export default {
+    methods: {
+        goback(){
+            this.$router.go(-1);
+        }
+    },
+};
+</script>
+<style lang="scss">
+.notfound {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -23,105 +23,108 @@ import Calendar from "@/pages/Dashboard/Calendar.vue";
 import store from '@/store';
 
 let authPages = {
-  path: "/",
-  component: AuthLayout,
-  name: "Authentication",
-  children: [
-    {
-      path: "/login",
-      name: "Login",
-      component: Login,
-      meta: {
-        allowAnonymous: true,
-      },
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: Register,
-      meta: {
-        allowAnonymous: true,
-      },
-    },
-    // {
-    //   path: "/lock",
-    //   name: "Lock",
-    //   component: Lock,
-    //   meta: {
-    //     allowAnonymous: true,
-    //   },
-    // }
-  ],
+    path: "/",
+    component: AuthLayout,
+    name: "Authentication",
+    children: [{
+            path: "/login",
+            name: "Login",
+            component: Login,
+            meta: {
+                allowAnonymous: true,
+            },
+        },
+        {
+            path: "/register",
+            name: "Register",
+            component: Register,
+            meta: {
+                allowAnonymous: true,
+            },
+        },
+        // {
+        //   path: "/lock",
+        //   name: "Lock",
+        //   component: Lock,
+        //   meta: {
+        //     allowAnonymous: true,
+        //   },
+        // }
+    ],
 };
 
-const routes = [
-  {
-    path: "/",
-    redirect: "/dashboard",
-    name: "Home",
-  },
-  authPages,
-  {
-    path: "/",
-    component: DashboardLayout,
-    children: [
-      {
-        path: "dashboard",
-        name: "Dashboard",
-        components: { default: Dashboard },
-      },
-      {
-        path: "user",
-        name: "Perfil",
-        components: { default: User },
-      },
-      {
-        path: "calendario",
-        name: "Calendario das Marcações",
-        components: { default: Calendar },
-      },
-      {
-        path: "marcacoes",
-        name: "Marcações",
-        components: { default: TableMarcacao },
-      },
-      {
-        path: "users",
-        name: "Utilizadores",
-        components: { default: UsersTable },
-        // beforeEnter(to, from, next) {
-        //   if (store.getters['userModule/restrictTo'](0)) {
-        //     next(); 
-        //   } else {
-        //     next('/');
-        //   }
-        // },
-      },
-      {
-        path: "licoes",
-        name: "Lições",
-        components: { default: LicoesTable },
-      },
-      {
-        path: "formacoes",
-        name: "Formações",
-        components: { default: FormacoesTable },
-      },
-      {
-        path: "formacoes-detalhe/:formacaoId",
-        name: "Formação Detalhe",
-        component: FormacaoDetalhe,
-        props: true,
-        // beforeEnter(to, from, next) {
-        //   if (store.getters['userModule/restrictTo'](0)) {
-        //     next(); 
-        //   } else {
-        //     next('/');
-        //   }
-        // },
-      },
-    ],
-  },
+const routes = [{
+        path: "/",
+        redirect: "/dashboard",
+        name: "Home",
+    },
+    authPages,
+    {
+        path: "/",
+        component: DashboardLayout,
+        children: [{
+                path: "dashboard",
+                name: "Dashboard",
+                components: { default: Dashboard },
+            },
+            {
+                path: "user",
+                name: "Perfil",
+                components: { default: User },
+            },
+            {
+                path: "calendario",
+                name: "Calendario das Marcações",
+                components: { default: Calendar },
+            },
+            {
+                path: "marcacoes",
+                name: "Marcações",
+                components: { default: TableMarcacao },
+            },
+            {
+                path: "users",
+                name: "Utilizadores",
+                components: { default: UsersTable },
+                // beforeEnter(to, from, next) {
+                //   if (store.getters['userModule/restrictTo'](0)) {
+                //     next(); 
+                //   } else {
+                //     next('/');
+                //   }
+                // },
+            },
+            {
+                path: "licoes",
+                name: "Lições",
+                components: { default: LicoesTable },
+            },
+            {
+                path: "formacoes",
+                name: "Formações",
+                components: { default: FormacoesTable },
+            },
+            {
+                path: "formacoes-detalhe/:formacaoId",
+                name: "Formação Detalhe",
+                component: FormacaoDetalhe,
+                props: true,
+                // beforeEnter(to, from, next) {
+                //   if (store.getters['userModule/restrictTo'](0)) {
+                //     next(); 
+                //   } else {
+                //     next('/');
+                //   }
+                // },
+            },
+            {
+                path: "*",
+                name: "Página não encontrada",
+                component: () =>
+                    import ("../pages/Dashboard/Pages/NotFound.vue")
+            }
+        ],
+    },
 ];
 
 export default routes;
